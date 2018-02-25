fdfbb9c0c92d9eabe29239eea19adb8256f333ae
lalepro <lauraleeprochaska@gmail.com>
2018-02-25 00:40:16 -0600

    can edit movies

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ const $ = require('jquery');
 
 const loader = require('./page-load.js');
 const {getMovies} = require('./api.js')
-// const title = document.getElementById('newMovie');
+// let id = document.getElementById('movieID');
 /*const rating = document.getElementById('rateForm');*/
+let id;
 
 let movieTitles = document.getElementById("movie-title");
 
@@ -18,10 +19,25 @@ function updatePage() {
             movieTitles.innerHTML +=
                 `
             <div id="movieDisplay">
-             ${id}. ${title} - Rating: ${rating} 
+             ${id}. ${title} - Rating: ${rating} <button id="del-btn-${id}" type="button" class="btn-danger col-1">X</button>
              </div>
             `;
-        })
+            // movieTitles.innerHTML +=
+            //     `
+            //     <button id="deleteButton-${id}" class="btn-primary col-2">Delete</button>
+            //     `
+        });
+
+        $('.btn-danger').click((e) => {
+            let id = event.currentTarget.id.split('-');
+            removeMovie(id[2]);
+            $('' + id[2]).hide();
+        });
+
+        // $("#deleteButton").click(() => {
+        //     // e.target.value;
+        //     removeMovie();
+        // })
     })
         .catch((error) => {
             alert('Oh no! Something went wrong.\nCheck the console for details.');
@@ -51,22 +67,42 @@ $("#movieButton").click((e) => {
         });
 });
 
+//=================================Allow users to EDIT a movie
+
+$("#editButton").click((e) => {
+    e.preventDefault();
+    const changeMovie = {title: $('#editMovie').val(), rating: $('#editForm').val(), id: $('#movieID').val()};
+    const url = '/api/movies/' + $('#movieID').val();
+    const options = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(changeMovie),
+    };
+    fetch(url, options)
+        .then(updatePage);
+});
+
+//====+==+========+=+=============+=+==+==DELETE A MOVIE
+
+function removeMovie(id) {
+         const options = {
+             method: 'DELETE',
+};
+    fetch(`/api/movies/${id}`, options)
+        .then(response => response.json())
+        .catch(error => console.log(error))
+
+}
+//
+// delMovies: (id) => {
+//     options.method = "DELETE";
+//     return fetch(`/api/movies/${id}`, options)
+//         .then(response => response.json())
+//         .catch(error => console.log(error))
+// },
+
 updatePage();
 
 
-
-
-
-
-
-// movieButton.addEventListener('click', () => {
-//     event.preventDefault();
-//     addMovie();
-// });
-
-// $('#addForm').on(movieButton, (e) => {
-//     const id = (movies.id.val());
-//     console.log($('#new-movie').val());
-//     e.preventDefault();
-//     addMovie();
-// });
