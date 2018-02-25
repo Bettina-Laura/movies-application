254c634341fed6b2a35ef33f83700878becd06fc
lalepro <lauraleeprochaska@gmail.com>
2018-02-24 18:59:16 -0600

    refactoring add movies method, still in process, no errors, but not updating html or db

--- src/index.js
+++ src/index.js
@@ -3,37 +3,73 @@ const bootstrap = require('bootstrap');
 const $ = require('jquery');
 
 const loader = require('./page-load.js');
-const  addMoviesHere = require('./add-movie.js');
-const {getMovies} = require('./api.js');
+const {getMovies} = require('./api.js')
+// const movieButton= document.getElementById('movieButton');
+const title = document.getElementById('newMovie');
+const rating = document.getElementById('rateForm');
 
 let movieTitles = document.getElementById("movie-title");
 
-getMovies().then((movies) => {
-    addMoviesHere.addNewMovie();
-    addMoviesHere.renderMovie();
-    addMoviesHere.renderMovies();
-    addMoviesHere.updateMovies();
-    addMoviesHere.interactiveKey();
+//-----------_--_-_-__-Allow users to add new movies
+$('#movieButton').click((e) => {
+    e.preventDefault();
+    const newestMovie = {title: title.val(), rating: rating.val()};
+    const url = '/api/movies/';
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newestMovie),
+    };
+    fetch(url, options)
+        .then( () => updatePage())
+        .catch((error) => {
+            alert('Oh no! Something went wrong.\nCheck the console for details.');
+            console.log(error);
 
-    loader.showPage();
-    console.log('Here are all the movies:');
-    console.log(addNewMovie);
+        });
+});
 
-    movies.forEach(({title, rating}) => {
-        // console.log(`id#${id} - ${title} - rating: ${rating}`);
-        movieTitles.innerHTML +=
-            `
-            <div>
-             ${title} - rating: ${rating} 
+updatePage();
+
+
+function updatePage() {
+    getMovies().then((movies) => {
+        loader.showPage();
+        console.log('Movies displaying on HTML');
+        movieTitles.innerHTML = ('');
+        movies.forEach(({title, rating, id}) => {
+            movieTitles.innerHTML +=
+                `
+            <div id="movieDisplay">
+             ${id}. ${title} - Rating: ${rating} 
              </div>
             `;
+        })
     })
-})
-    .catch((error) => {
-    alert('Oh no! Something went wrong.\nCheck the console for details.');
-    console.log(error);
-});
-// {addNewMovie, renderMovie, renderMovies, updateMovies, interactiveKey}
+        .catch((error) => {
+            alert('Oh no! Something went wrong.\nCheck the console for details.');
+            console.log(error);
+        });
+}
 
 
 
+
+
+
+
+
+
+// movieButton.addEventListener('click', () => {
+//     event.preventDefault();
+//     addMovie();
+// });
+
+// $('#addForm').on(movieButton, (e) => {
+//     const id = (movies.id.val());
+//     console.log($('#new-movie').val());
+//     e.preventDefault();
+//     addMovie();
+// });
