ea1d44d21ce36d35449ea9734968f9f3b5017d9b
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-02-26 18:19:59 -0600

    made search input functional

--- src/index.js
+++ src/index.js
@@ -98,3 +98,31 @@ function removeMovie(id) {
 
 updatePage();
 
+//SEARCH MOVIES
+
+
+function searchMovie() {
+// Declare variables
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("movieDisplay");
+    tr = table.getElementsByTagName("tr");
+// var searchCategory = (radio button input value)
+//     var userChoice = searchCategory ? 2 : 1;
+// Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        console.log("td.innerHTML", td);
+        if (td) {
+            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+
+
+}
+$('#myInput').on('keyup', searchMovie);
